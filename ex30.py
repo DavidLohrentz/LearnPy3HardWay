d58be4fdc4054c8f4f142d92515edfbd904a101f
David Lohrentz <david_lohrentz@yahoo.com>
2017-11-23 20:21:30 -0600

    Added user input on no. of cars & trucks

--- ex30.py
+++ ex30.py
@@ -16,24 +16,23 @@ if trucks == 0:
 
 
 if trucks / cars > 1.5:
-    print("That's too many mother-fucking trucks.")
+    print("That's too many trucks.")
 elif trucks < cars:
-    print("Yo, dipshit! Maybe we should take the trucks.")
+    print("Yo! Maybe we should take the trucks.")
 else:
     print("We can't decide.")
 
+ratio = people / trucks
 if people / trucks < 2 and people / cars > 3:
-    print(f"""The people to truck ratio is {truck_ratio}.\n
-    Let's just take the damn trucks.""")
+    print(f"The people-to-truck ratio is {ratio}, so let's just take the trucks.")
 elif people / cars <= 3.2 and people / cars >= 3.1:
     print("We have about Pi cars. Let's make a circle.")
 else:
     print("Hey. I need pizza now!")
 
 if cars > people:
-    print("Hey dumbass, we should take the cars.")
+    print("Perhaps, we should take the cars.")
 elif people > (cars + trucks) and cars < people:
-    print("""We have more people than vehicles,
-    \nso we have to car pool with both cars and trucks.""")
+    print("""We have more people than vehicles, so we need both cars and trucks.""")
 else:
-    print("We still can't fucking decide. I suggest unicycles.")
+    print("We still can't decide. I suggest unicycles.")
