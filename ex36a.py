a0a7d320d5a0b0a009271e14599161d94864d4f9
RichInBunlyGoodness <31279399+RichInBunlyGoodness@users.noreply.github.com>
2017-11-28 16:51:40 -0600

    Put dooshbag list in sep file

--- ex36a.py
+++ ex36a.py
@@ -1,15 +1,16 @@
 import time
 from sys import exit
 from random import randint
-# make a long list of dooshbags
-# pull one random name from the list to dispatch with so game has a lot of variation
-mylist = ["Tom Cruise", "Jean-Claude Van Damm", "Steven Seagal", "David Hasselhoff",
-"Sarah Palin", "Ted Cruz", "Michele Bachmann", "Newt Gingrich", "Geraldo Rivera", "Tucker Carlson",
-"Tom Crean", "P.J. Fleck"]
+# make a long list of dooshbags from dooshlist.txt file
+
+mylist = open("dooshlist.txt").readlines()
 c = len(mylist)
 a = randint(0,c)
 b = mylist[a]
-print(f"\nRandom dooshbag: {b}")
+initials = ''.join(name[0].lower() for name in b.split())
+print(b)
+print(initials)
+
 # first pop up
 tool = input("\ntool: ")
 status = input("status: ")
@@ -20,15 +21,15 @@ def broken_zuc(whack, tool, status, drunkenness):
     print(whack, tool, status, drunkenness)
     exit(0)
 
-print("The heads of Tom Crean and Chuck Norris pop up.\n")
+print(f"The heads of {b} and Chuck Norris pop up.\n")
 time.sleep(2)
-whack = input(f"Who do you whack tc)\ncn)\n>>> ")
+whack = input(f"Who do you whack?\n{b} ({initials})\nChuck Norris (cn)\n>>> ")
 print(f"You whack {whack}.")
-if whack == "tc" and tool == "zuccini" and status == "long" and drunkenness == "none":
+if whack == (b or initials) and tool == "zuccini" and status == "long" and drunkenness == "none":
     print(whack, tool, status, drunkenness)
-    print("You whack Crean so hard that your zuccini breaks.")
+    print(f"You whack {b} so hard that your zuccini breaks.")
     time.sleep(2)
-    print("Crean runs away sucking his thumb.\n")
+    print(f"{b} staggers away in fear.\n")
     status = "short"
     whack = "cn"
     broken_zuc(whack, tool, status, drunkenness)
@@ -37,7 +38,7 @@ elif whack == "cn" and tool == "zuccini" and status == "long" and drunkenness ==
     print(whack, tool, status, drunkenness)
     print("Before you can whack him, Chuck Norris breaks your zuccini in half with a roundhouse kick.")
     time.sleep(2)
-    print("You fail to eliminate Cuck Norris. However he gets rid of Crean with a roundhouse kick.\n")
+    print(f"You fail to eliminate Cuck Norris. However he gets rid of {b} with a roundhouse kick.\n")
     status = "short"
     broken_zuc(whack, tool, status, drunkenness)
 
@@ -45,7 +46,7 @@ elif whack == "cn" and tool == "zuccini" and status == "long" and drunkenness ==
     print(whack, tool, status, drunkenness)
     print("You are so drunk that you fall down before Norris hits you with a roundhouse kick.")
     time.sleep(2)
-    print("The wind vortex from his kick blows away Crean and breaks your zuccini in half.\n")
+    print(f"The wind vortex from his kick blows away {b} and breaks your zuccini in half.\n")
     status = "short"
     broken_zuc(whack, tool, status, drunkenness)
 
