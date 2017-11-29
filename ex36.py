600f4cd168dad7367d70bc198b815d5c71d38acb
David Lohrentz <david_lohrentz@yahoo.com>
2017-11-28 20:49:44 -0600

    Merged everything from ex36a.py

--- ex36.py
+++ ex36.py
@@ -1,11 +1,19 @@
 # Whackamole
 import time
 from sys import exit
+from random import randint
+# make a list of dooshbags from dooshlist.txt file
+mylist = open("dooshlist.txt").readlines()
+c = len(mylist)
+a = randint(0,c)
+b = mylist[a]
+random_doosh = b.rstrip()
+initials = ''.join(name[0].lower() for name in b.split())
+print(f"\nRandom doosh is {random_doosh} & initials are {initials}.") # debug line
+print(f"\n{c} dooshes in doosh list.") # debug line
 
 def dream():
-    print("After a weird day at work and way too much to drink,")
-    time.sleep(2.5)
-    print("you fall asleep and find yourself in the most vivid dream you have ever experienced.\n")
+    print("After a VERY weird day at work and you got out for drink.\n")
     time.sleep(2.5)
     booze = input("How many bottles of beer did you drink?  ")
 
@@ -25,7 +33,19 @@ def dream():
         drunkenness = "buzzed"
     else:
         drunkenness = "sloshed"
-    print("What you see in front of you is a giant game of weird whackamole.\n")
+    print("you fall asleep and begin to dream.\n")
+    time.sleep(2.5)
+    print("You find yourself at an arcade game room.")
+    time.sleep(2)
+    print("The price of admission is to enter the name of a dooshbag.")
+    new_doosh = input(">>> ")
+    new_doosh = new_doosh + '\n'
+    f = 'dooshlist.txt'
+    file = open(f, 'a+')
+    file.write(new_doosh)
+    file.close()
+
+    print("In front of you is a giant game of weird whackamole.\n")
     time.sleep(2.5)
     print("Fran Drescher is laughing at you and telling you to \'select your whacking tool.\'\n")
     time.sleep(3)
@@ -76,14 +96,49 @@ def second_tool(drunkenness):
         print("Dude, wtf? Try again.\n")
         second_tool(drunkenness)
 
+def broken_zuc(whack, tool, status, drunkenness):
+    print("You have arrived at broken_zuc function.")
+    print(whack, tool, status, drunkenness)
+    exit(0)
+
 def smack(tool, status, drunkenness):
     print("You have arrived at the smack function.") #debug
     print(f"\ntool: {tool} \nstatus: {status} \ndrunkenness: {drunkenness}\n\n") #debug
-    first_whack = input(f"Straight away, Tom Crean and Roy Moore pop up. Which one do you whack with your {tool}? >>> ")
-    print(f"Your first whack is {first_whack}")
-    exit(0)
 
+    print(f"The heads of {random_doosh} and Chuck Norris pop up.\n")
+    time.sleep(2)
+    whack = input(f"Who do you whack?\n{random_doosh} ({initials})\nChuck Norris (cn)\n>>> ")
+    print(f"You whack {whack}.")
+    if whack == (random_doosh or initials) and tool == "zuccini" and status == "long" and drunkenness == "none":
+        print(whack, tool, status, drunkenness)
+        print(f"You whacked {random_doosh} so hard that your zuccini broke.")
+        time.sleep(2)
+        print(f"{random_doosh} staggers away in fear.\n")
+        status = "short"
+        whack = "cn"
+        broken_zuc(whack, tool, status, drunkenness)
 
+    elif whack == "cn" and tool == "zuccini" and status == "long" and drunkenness == "none":
+        print(whack, tool, status, drunkenness)
+        print("Before you can whack him, Chuck Norris breaks your zuccini in half with a roundhouse kick.")
+        time.sleep(2)
+        print(f"You failed to eliminate Cuck Norris. However he gets rid of {random_doosh} with a roundhouse kick.\n")
+        status = "short"
+        broken_zuc(whack, tool, status, drunkenness)
+
+    elif whack == "cn" and tool == "zuccini" and status == "long" and drunkenness == "sloshed":
+        print(whack, tool, status, drunkenness)
+        print("You are so drunk that you fell down before Norris hit you with his roundhouse kick.")
+        time.sleep(2)
+        print(f"The wind vortex from his kick blew away {random_doosh} and broke your zuccini in half.\n")
+        status = "short"
+        broken_zuc(whack, tool, status, drunkenness)
+
+    else:
+        print("Error")
+        exit(0)
+
+# ---------------------------------------------------------------
 def cobra(drunkenness):
     print("You have arrived at the cobra function.") #debug
     print("Congratulations! You get to do your whacking with a 12 foot king cobra.\n")
