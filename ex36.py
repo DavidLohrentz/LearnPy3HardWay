61bd0ab887c8a6b1ea1d1405e5426a065aea108f
David Lohrentz <david_lohrentz@yahoo.com>
2017-12-03 20:53:20 -0600

    Started adding comment lines

--- ex36.py
+++ ex36.py
@@ -1,9 +1,10 @@
 # Whackamole
-import time
+import time   # time.sleep(x) to pause during the playback
 from sys import exit
-from random import choice, randint
+from random import choice, randint # choice(listname) takes a random item from list
 import os
-    # make a list of dooshbags from dooshlist.txt filed
+
+# this should not be here, but is a dumb way of fixing errors
 global your_name
 global tool_list
 global tool_to_check
@@ -17,17 +18,20 @@ global none_walk
 global snake_descriptors
 
 
-quip_list = ["", "", "You are going to regret picking this one.", "How dumb are you to pick the this option?", "Lucky choice.", "Better luck next time."]
-snake_descriptors = ["hungry", "hissing", "slithering", "friendly", "comedic", "pancake-flipping", "deadly"]
+# The code pulls random words from these lists to change the story every time
+quip_list = ["", "", "WTF, Dude?", "You are going to regret picking this one.", "How dumb are you to pick this option?", "Lucky choice.", "Better luck next time."]
+snake_descriptors = ["hungry", "nocturnal", "hissing", "slithering", "friendly", "comedic", "pancake-flipping", "deadly", "cunning"]
 word_list = ["a demoralizing", "an exciting", "a sordid", "a discouraging", "an upsetting", "a soul-crushing", "a depressing", "a thrilling", "a dangerous", "a job-killing", "a sensational", "a rip-roaring", "an electrifying", "a titilating", "an arousing"]
 friend_types = ["sketchiest", "most excitable", "jumpiest", "ugliest", "most conspiratorial", "most doinkable", "most flamboyant", "weirdest", "most bizarre", "most desperate", "loneliest", "fishiest"]
 none_walk_list = ["wander", "tip-toe", "waltz", "amble", "romp", "yawn", "sing", "saunter", "meander", "mosey", "stroll"]
 buzzed_walk_list = ["dance", "frolic", "skip", "cartwheel", "prance", "duckwalk", "scamper", "strut", "sashay"]
 sloshed_walk_list = ["stumble", "stagger", "crawl", "wobble", "lurch", "dodder", "puke", "flounder"]
 none_pause = ["tie your shoes", "write down an outline of your new novel", "clean the dogshit from your shoes", "turn your rally cap inside out"]
-buzzed_pause = ["hug everybody", "sing the national anthem", "take a selfie", "make an omelette"]
+buzzed_pause = ["hug everybody", "sing the national anthem", "take a selfie", "make an omelette", "start a fire", "pluck a chicken"]
 sloshed_pause = ["take a piss", "puke on your shoes", "launch a supersonic fart", "do a kegstand", "scream, I\'m mad as hell, and I\'m not going to take it anymore."]
-snake_list = ["rattlesnake", "king cobra", "black mamba", "death adder", "puff adder", "spitting cobra", "water moccasin", "Yellow Belly Sea Snake", "Inland Taipan", "Anaconda", "Tiger Snake"]
+snake_list = ["rattlesnake", "Copperhead", "king cobra", "mamushi", "black mamba", "death adder", "puff adder", "spitting cobra", "water moccasin", "Yellow Belly Sea Snake", "Inland Taipan", "Anaconda", "Tiger Snake"]
+door_list = ["a grungy", "a ramshackle", "an inviting", "a hidden", "a secret", "a disguised", "an invisible"]
+status_list = [["highfalutin", "under-rated"], ["turgid", "humble"], ["greasey", "slimey"], ["bloody", "poopy"], ["chocolate-covered", "beer-battered"], ["tiny", "gargantuan"], ["pink", "blue"], ["dirty", "rotten"], ["long", "short"], ["old", "kaput"], ["burning", "holey"], ["squirming", "biting"], ["creepy", "nauseating"], ["surly", "ticklish"], ["frisky", "lethargic"], ["raw", "cooked"], ["flat", "roundish"], ["epic", "dwarfish"], ["soft", "fragile"], ["bloated", "emaciated"], ["leaky", "ebola-tinged"], ["Russian", "Turkish"], ["operatic", "shrill"], ["wide", "narrow"]]
 
 def file_accessible(filepath, mode):
     # check if a file exists and is accessible.
@@ -47,21 +51,21 @@ def check_if_files_exist():
         exit(0)
 
     else:
-        print("\'dooshlist.txt\' exists in this directory.")
+        goodfiles = 1
 
     if tool_list_accessible == False:
         print("You need to create a txt file named \'tool_list.txt\' in this directory.")
         exit(0)
 
     else:
-        print("\'tool_list.txt\' exists in this directory.")
+        goodfiles += 1
 
     if moods_accessible == False:
         print("You need to create a txt file named \'moods.txt\' in this directory.")
         exit(0)
 
     else:
-        print("\'moods.txt\' exists in this directory.\n")
+        print("All three support files exist in this directory.\n\n")
 
 def add_doosh(new_doosh):
     new_doosh = new_doosh + '\n'
@@ -83,6 +87,10 @@ def word_generator():
     random_word = choice(word_list)
     return random_word
 
+def door_type():
+    random_door = choice(door_list)
+    return random_door
+
 def tool_pick_quip():
     random_quip = choice(quip_list)
     return random_quip
@@ -95,19 +103,24 @@ def friend_type_generator():
     friend_type = choice(friend_types)
     return friend_type
 
-def doosh_generator():
-    mylist = open("dooshlist.txt").readlines()
-    b = choice(mylist)
-    random_doosh = b.rstrip()
-    initials = initial_maker(random_doosh)
+def doosh_generator(): # get a randome doosh from the dooshlist.txt file
+    get_doosh = open("dooshlist.txt", 'r')
+    mylist = get_doosh.readlines()
+    b = choice(mylist) # pull a random list item from mylist
+    random_doosh = b.rstrip() # remove \n from end of line
+    initials = initial_maker(random_doosh) # initialize random_doosh
+    get_doosh.close() # close the file
 
     return random_doosh, initials
 
 def mood_generator():
-    a = open('moods.txt').readlines()
+    # a = open('moods.txt').readlines()
+    moodfile =  open('moods.txt', 'r')
+    a = moodfile.readlines()
     random_mood = choice(a)
     random_mood = random_mood.rstrip()
     mood_initials = initial_maker(random_mood)
+    moodfile.close()
 
     return random_mood, mood_initials
 
@@ -116,11 +129,13 @@ def initial_maker(name_to_be_initialled):
     return myinitials
 
 def tool_generator():
-    tool_list= open('tool_list.txt').readlines()
+    get_tool = open('tool_list.txt', 'r')
+    pick_tool = get_tool.readlines()
     global random_tool
-    random_tool = choice(tool_list)
+    random_tool = choice(pick_tool)
     random_tool = random_tool.rstrip()
     tool_initials = initial_maker(random_tool)
+    get_tool.close()
 
     return random_tool, tool_initials
 
@@ -137,7 +152,6 @@ def status_generator():
     global random_status1
     global random_status2
 
-    status_list = [["highfalutin", "under-rated"], ["turgid", "humble"], ["greasey", "slimey"], ["bloody", "poopy"], ["chocolate-covered", "beer-battered"], ["tiny", "gargantuan"], ["pink", "blue"], ["dirty", "rotten"], ["long", "short"], ["old", "kaput"], ["burning", "holey"], ["squirming", "biting"], ["creepy", "nauseating"], ["surly", "ticklish"], ["frisky", "lethargic"], ["raw", "cooked"], ["flat", "roundish"], ["epic", "dwarfish"], ["soft", "fragile"], ["bloated", "emaciated"], ["leaky", "ebola-tinged"], ["Russian", "Turkish"], ["operatic", "shrill"], ["wide", "narrow"]]
     random_status = choice(status_list)
     random_status1 = random_status[0]
     random_status2 = random_status[1]
@@ -161,7 +175,7 @@ def dream():
     drinks()
 
 def drinks():
-    booze = input(f"{your_name}, how many drinks did you have with {friend_name}?  ")
+    booze = input(f"{your_name}, how many drinks did you have with {friend_name}?  \n")
     global drunkenness
     goldnum = []
     a = ""
@@ -194,19 +208,20 @@ def drinks():
     print(f"After you have {cooked} drinks with {friend_name}, you fall into a deep sleep and begin to dream.\n")
     time.sleep(2.5)
     arcade_descriptor = word_generator()
-    print(f"You {drunkwalk} down a long stairs to {arcade_descriptor} old school arcade room.\n")
+    print(f"You {drunkwalk} down a very long stairs into the darkness.\n")
     time.sleep(2)
-    print(f"Half way down the stairs you pause to {pause_do}.")
-    time.sleep(1.5)
-    print("You get to the bottom of the stairs, and open the green door.")
+    print(f"Half way down the stairs you pause to {pause_do}.\n")
+    time.sleep(2)
+    door_word = door_type()
+    print(f"You get to the bottom of the stairs, and open {door_word} door.\n")
     time.sleep(2)
 
-    print(f"In front of you, {your_name}, is the game of Celebrity Dooshbag Whackamole.\n")
-    time.sleep(2.5)
+    print(f"In front of you, is the game of Celebrity Dooshbag Whackamole.\n")
+    time.sleep(2)
 
     print(f"{friend_name} is laughing at you to \'select your whacking tool.\'\n")
     time.sleep(3)
-    print(f"{your_name}, you will receive three whacking tool options, picking yes or no, one at a time.\n")
+    print(f"{your_name}, you will now receive three whacking tool options, picking yes or no, one at a time.\n")
     #time.sleep(2.5)
     tool_pick(drunkenness)
 
@@ -214,13 +229,12 @@ def tool_pick(drunkenness):
     random_tool, tool_initials = tool_generator()
     your_pick = input(f"Your first whacking option is a {random_tool}. Do you take this whacking tool, {your_name}?\ny) yes\nn) no\n>>> \n")
 
-    # print(f"your_pick is \'{your_pick}\'.") #debug
     if your_pick == "y" or your_pick == "yes":
         quip = tool_pick_quip()
         time.sleep(2)
         print(quip)
         random_status1, random_status2, status1_initials, status2_initials = status_generator()
-        print(f"\n\nChose one, {your_name}: \n{random_status1} {random_tool} ({status1_initials})")
+        print(f"\n\nChoose one, {your_name}: \n{random_status1} {random_tool} ({status1_initials})")
         print(f"{random_status2} {random_tool} ({status2_initials})")
         status_answer = input("\n >>> ")
         if status_answer == status1_initials:
@@ -237,6 +251,7 @@ def tool_pick(drunkenness):
         tool_pick(drunkenness)
     else:
         exit(0)
+
 def check_tool(tool_to_check):
     if tool_to_check == random_tool:
         random_tool2, tool_initial2 = tool_generator()
@@ -246,9 +261,9 @@ def check_tool(tool_to_check):
 
 def rand_joke():
     a = randint(0, 19)
-    if a == 0:
+    if a == 10:
         # Pops up about 5% of the time.
-        return f"{your_name}, we checked your references and it seems you have no friends. We'll let you keep pretending you do have one."
+        return "we checked your references and it seems you have no friends. We'll let you keep pretending you do have one."
     else:
         return ""
 
@@ -308,16 +323,13 @@ def smack(tool, status, drunkenness):
     time.sleep(2)
     whack = input(f"Who do you whack with your {tool}?\n{random_doosh_mood} {random_doosh} ({initials})\nChuck Norris (cn)\n>>> ")
     # print(f"You whack {whack}.") #debug
+    print(f"\nWho is whack? {whack}.\n")
 
     if status == "dipshit":
         print("You failed to answer correctly. Let this be a lesson to you.")
         time.sleep(2)
         print(f"{random_doosh} and Chuck Norris take away your {tool} and whack you until you have no arms or legs.\n")
         exit(0)
-
-    elif any(tool in s for s in snake_list):
-        print(f"Your {tool} bites Chuck Norrris in the face and that was that.")
-        popup2(random_doosh, tool, status, drunkenness)
 
     elif drunkenness == "sloshed":
         sloshedwhack(whack, tool, status)
@@ -334,17 +346,12 @@ def smack(tool, status, drunkenness):
 
 def sloshedwhack(whack, tool, status):
     if whack == initials:
-        print(f"After you fall down drunk, {random_doosh} and Chuck Norris fight to the death.\n")
+        print(f"You fall down drunk, narrowly avoiding a roundhouse kick from Chuck Norris.\n")
         time.sleep(2)
-        print(f"Getting drunk and attempting to whack {random_doosh} with a {status} {tool} was the winning answer.\n")
-        time.sleep(1)
-        print("Congratulations! You are the winner. Go forth and prosper.\n")
-        time.sleep(1)
-        doosh_prize = input("Your prize is adding a dooshbag to the list: ")
-        add_doosh(doosh_prize)
-        time.sleep(1)
-        print(f"For a successful life, continue to drink heavily and whack random celebrity dooshbags with a {status} {tool}.\n")
-        exit(0)
+        print(f"{random_doosh} was not so lucky. Chuck Norris kicks {random_doosh} into another dimension.\n")
+        time.sleep(2)
+        popup2('Chuck Norris', tool, status, drunkenness)
+
 
     elif whack == "cn":
         print("Nobody takes on Chuck Norris when they are drunk.\n")
@@ -367,7 +374,12 @@ def sloshedwhack(whack, tool, status):
         exit(0)
 
 def buzzedwhack(whack, tool, status):
-    if whack == initials:
+
+    if any(tool in s for s in snake_list):
+        print(f"Your {tool} bites Chuck Norrris in the face while it roundhouse kicks your {tool}. They are both dead.")
+        popup2(random_doosh, tool, "dead", drunkenness)
+
+    elif whack == initials:
         time.sleep(1.5)
         print(f"Lucky for you, {random_doosh} has a {tool} fetish.\n")
         time.sleep(2)
@@ -397,7 +409,7 @@ def buzzedwhack(whack, tool, status):
         popup2("Chuck Norris", tool, status, drunkenness)
 
     else:
-        print("teetotal error")
+        print("buzzedwhack error")
         exit(0)
 
 
@@ -430,10 +442,8 @@ def teetotalwhack(whack, tool, status):
 
 def snake_tool(drunkenness):
     # print("You have arrived at the snake_tool function.") #debug
-    time.sleep(1)
     get_snake, snake_initials = snake_generator()
     status = snake_word_generator()
-    print(f"\nCongratulations!")
     time.sleep(2)
     print(f"You have won the opportunity to do your whacking with a {status} {get_snake}.\n")
     time.sleep(2.5)
@@ -488,15 +498,15 @@ def popup2(whack, tool, status, drunkenness):
 
     elif drunkenness == "sloshed":
         message = word_generator()
-        print(f"You are {message} drunk.\n")
+        print(f"You are {message} drunk. You lose.\n")
         time.sleep(2)
-        print("You must be from Wisconsin. Better luck next time.")
+        print("Better luck next time.")
         exit(0)
 
     else:
-        print(f"{level2_doosh} convinces you to take up Gandhian nonviolence rather than whacking your way through life with a {status} {tool}.")
+        print(f"{level2_doosh} convinces you to take up Gandhian nonviolence rather than whacking your way through life with a {status} {tool}.\n")
         time.sleep(2)
-        print(f"\nYou put down your {status} {tool}, and never pick up another one as long as you live.")
+        print(f"\nPut down your {status} {tool}, and never pick up another one as long as you live.")
         exit(0)
 
 def third_whack():
@@ -506,7 +516,12 @@ def third_whack():
     print(f"{level3_doosh} pops up through a new hole.\n")
     pick_one = input(f"Do you intend to whack {level3_doosh}? \n(y) (yes) \n(n) (no)\n>>> ")
     time.sleep(2)
-    print("That was the winning answer. Go have a whackless life.")
+    print("That was the winning answer. You clearly have a whacky life.\n")
+    time.sleep(2)
+    doosh_prize = input("Your prize is adding a dooshbag to the list: ")
+    add_doosh(doosh_prize)
+    print(f"\nFor a successful life, continue to drink heavily and whack random celebrity dooshbags with a {status} {tool}.\n")
+    exit(0)
 
 
 check_if_files_exist()
