95231060875fe898298f184674f564a694d71c93
David Lohrentz <david_lohrentz@yahoo.com>
2018-01-13 23:27:04 -0600

    Updated for new version python

--- pydocker.sh
+++ pydocker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 dir=$(pwd -P)
-sudo docker run -it --name learn-p --rm -v $dir:/var/opt python:3.6.4-alpine3.7 /bin/sh
+sudo docker run -it --name learn-p --rm -v $dir:/var/opt python:3.7-rc-alpine /bin/sh
